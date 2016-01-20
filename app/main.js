f2e0f1da16ab8bdd8b752777decb91a8fceba9ea
Katie Wright & Nilushanan Kulasingham <pair+KatieWright26.niluk@devbootcamp.com>
2016-01-20 15:28:58 +1300

    Push up

--- app/main.js
+++ app/main.js
@@ -1,27 +1,27 @@
 import { filter } from 'ramda'
 
-const wins = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+var board = ['', '', '', '', '', '', '', '', '']
 
-const winner = ['X', 'O', 'X', 'O', 'X', '', 'X', '', 'O']
+const wins = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+  [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+]
 
 const isAWin = function (board, player, win) {
   return filter(function(i) {return board[i] === player }, win).length === 3
 }
 
-console.log("Top row: " + isAWin(winner, 'X', wins[0]))
-console.log("Middle row: " + isAWin(winner, 'X', wins[1]))
-console.log("Bottom row: " + isAWin(winner, 'X', wins[2]))
-console.log("Left column: " + isAWin(winner, 'X', wins[3]))
-console.log("Middle column: " + isAWin(winner, 'X', wins[4]))
-console.log("Right column: " + isAWin(winner, 'X', wins[5]))
-console.log("Top left to bottom right: " + isAWin(winner, 'X', wins[6]))
-console.log("Top right to bottom left: " + isAWin(winner, 'X', wins[7]))
+const playerWins = function (board, player) {
+  return filter((win) => isAWin(board, player, win), wins).length > 0
+}
+
+const winner = ['X', 'O', 'X', 'O', 'X', '', 'X', '', 'O']
+
+console.log("Is O a winner? " + playerWins(winner, 'O'))
+console.log("Is X a winner? " + playerWins(winner, 'X'))
+
+const whoseMoveIsIt = function (moves) {
+  return (moves.length % 2 === 0) ? 'X' : 'O'
+}
+
+const makeMoves = function (board )
